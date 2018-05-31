f846513f40e7f39a3b73ff68412164833609de1b
LTKSK <komari0714@gmail.com>
2018-05-31 23:15:55 +0900

    [add] MaterialとMeshのcpp作成

---
+++ source/Material.cpp
@@ -0,0 +1,141 @@
+#include "Material.h"
+
+
+namespace LtRenderer
+{
+Material::Material(Vec3& albedo, Vec3& emission) : _albedo(albedo), _emission(emission) {}
+Material::~Material() {}
+
+bool Material::isEmissive()
+{
+	return _emission.x() > 0.0 || _emission.y() > 0.0 || _emission.z() > 0.0;
+}
+
+Vec3 Material::albedo()
+{
+	return _albedo;
+}
+
+Vec3 Material::emission()
+{
+	return _emission;
+}
+
+Vec3 Material::orientingNormal(Ray ray, Vec3 normal) const
+{
+	return dot(normal, ray.direction()) < 0.0 ? normal : -normal;
+}
+
+/*
+Lambertion
+*/
+
+Lambertion::Lambertion(Vec3& albedo, Vec3& emission) : Material(albedo, emission) {}
+
+Lambertion::~Lambertion() {}
+
+Ray Lambertion::scatter(const Ray& ray, const Vec3& hit_position, const Vec3& normal, Vec3& attenuation, Random* random) const
+{
+	Vec3 orienting_normal = orientingNormal(ray, normal);
+	Vec3 u, v, w;
+	w = orienting_normal;
+	if (abs(w.x()) > 0.1)
+	{
+		u = normalize(cross(Vec3(0.0, 1.0, 0.0), w));
+	}
+	else
+	{
+		u = normalize(cross(Vec3(1.0, 0.0, 0.0), w));
+	}
+	v = cross(w, u);
+	double r1 = D_PI * 2 * random->zeroToOneFloat();
+	double r2 = random->zeroToOneFloat();
+	double r2s = sqrt(r2);
+
+	Vec3 diffuse_direction = normalize(u * cos(r1) * r2s + v * sin(r1) * r2s + w * sqrt(1.0 - r2));
+
+	attenuation *= _albedo;
+	return Ray(hit_position + diffuse_direction*0.01f, diffuse_direction);
+}
+
+std::string Lambertion::materialType() const
+{
+	return _material_type;
+}
+
+/*
+Metal
+*/
+
+Metal::Metal(Vec3& albedo, Vec3& emission) : Material(albedo, emission) {}
+
+Metal::~Metal(){}
+
+Ray Metal::scatter(const Ray& ray, const Vec3& hit_position, const Vec3& normal, Vec3& attenuation, Random* random) const
+{
+	Vec3 reflect_direction = normalize(reflect(ray.direction(), normal));
+	attenuation *= _albedo;
+	return Ray(hit_position + reflect_direction * 0.01f, reflect_direction);
+}
+
+std::string Metal::materialType() const
+{
+	return _material_type;
+}
+
+/*
+Metal
+*/
+
+Dielectric::Dielectric(Vec3& albedo, Vec3& emission, double ior) : Material(albedo, emission) {}
+
+Dielectric::~Dielectric() {}
+
+Ray Dielectric::scatter(const Ray& ray, const Vec3& hit_position, const Vec3& normal, Vec3& attenuation, Random* random) const
+{
+	// rayが内側からオブジェクトに交差した場合にはnormalを反転させる
+	Vec3 orienting_normal = dot(normal, ray.direction()) < 0.0 ? normal : -normal;
+	bool into = dot(normal, orienting_normal) > 0.0;
+	double air_ior = 1.0;
+	double ior_ratio = into ? air_ior / _ior : _ior / air_ior;
+	double dot_dn = dot(ray.direction(), orienting_normal);
+	double D = 1.0 - ior_ratio * ior_ratio * (1.0 - dot_dn * dot_dn);
+
+	//Dが0.0より小さい場合、屈折後のベクトルとnormalの角度が90を超えているので全反射として扱う
+	if (D < 0.0)
+	{
+		attenuation *= _albedo;
+		Vec3 reflect_direction = normalize(reflect(ray.direction(), orienting_normal));
+		return Ray(hit_position + reflect_direction * 0.01f, reflect_direction);
+	}
+
+	//schlickの近似を用いて、屈折光の運ぶ光の割合を求める
+	double f0 = pow2((_ior - air_ior) / (_ior + air_ior));
+	double cosine = into ? -dot_dn : dot(ray.direction(), normal);
+	double re = f0 + (1 - f0) * pow5(1 - cosine);
+	double reflect_prob = 1.0 - re;
+	//屈折時の放射輝度の変化は屈折率の比の二乗
+	double ior_ratio2 = pow2(ior_ratio);
+	double tr = reflect_prob * ior_ratio2;
+	double prob = re * 0.5 + 0.25;
+	//結果を乱数で分岐する
+	if (prob > random->zeroToOneFloat())
+	{
+		//反射方向のサンプリング
+		attenuation *= _albedo * re / prob;
+		Vec3 reflect_direction = normalize(reflect(ray.direction(), orienting_normal));
+		return Ray(hit_position + reflect_direction * 0.01f, reflect_direction);
+	}
+	//屈折方向のサンプリング
+	attenuation *= _albedo * tr / (1.0 - prob);
+	Vec3 refract_direction = normalize((ray.direction() * ior_ratio) - (orienting_normal * (ior_ratio * dot_dn + sqrt(D))));
+	return Ray(hit_position + refract_direction * 0.01f, refract_direction);
+}
+
+std::string Dielectric::materialType() const
+{
+	return _material_type;
+}
+
+
+}//namespace LtRenderer
---
+++ source/Mesh.cpp
@@ -0,0 +1,219 @@
+#include "Mesh.h"
+
+
+namespace LtRenderer
+{
+Mesh::Mesh() {}
+Mesh::~Mesh() {}
+
+/*
+Sphere
+*/
+
+Sphere::Sphere(Vec3 center, double radius, Material* material) : _position(center), _radius(radius), _material(material)
+{
+	double max_x = center.x() + radius;
+	double max_y = center.y() + radius;
+	double max_z = center.z() + radius;
+	double min_x = center.x() - radius;
+	double min_y = center.y() - radius;
+	double min_z = center.z() - radius;
+	_bounding_box = new AABB(Vec3(max_x, max_y, max_z), Vec3(min_x, min_y, min_z));
+	_surface_area = (4.0 * D_PI * radius * radius);
+}
+
+Sphere::~Sphere()
+{
+	delete _bounding_box;
+	delete _material;
+}
+
+Material* Sphere::material() const
+{
+	return _material;
+}
+
+AABB* Sphere::boundingBox() const
+{
+	return _bounding_box;
+}
+
+Vec3 Sphere::position() const
+{
+	return _position;
+}
+
+double Sphere::surfaceArea() const
+{
+	return _surface_area;
+}
+
+Vec3 Sphere::randomPoint(Random* random) const
+{
+	const auto r1 = D_PI * 2.0 * random->zeroToOneFloat();
+	const auto r2 = random->zeroToOneFloat();
+	const auto r2s = sqrt(r2);
+	return _position + (_radius + D_HIT_MIN) * Vec3(sqrt(1.0 - r2 * r2) * cos(r1), sqrt(1.0 - r2 * r2) * sin(r1), r2);
+}
+
+bool Sphere::intersect(const Ray& ray, Intersection *intersection) const
+{
+	auto p = _position - ray.origin();
+	auto b = dot(p, ray.direction());
+	auto det = b * b - dot(p, p) + _radius * _radius;
+	if (det >= 0.0)
+	{
+		auto sqrt_det = sqrt(det);
+		auto t1 = b - sqrt_det;
+		auto t2 = b + sqrt_det;
+		//t1,t2のいずれかが、衝突時の値が閾値より大きく、なおかつ既にhitしている他のobjectより小さい場合にtrue
+		if (t1 > D_HIT_MIN && intersection->t >= t1)
+		{
+			intersection->setNormal(normalize(ray.pointAtParameter(t1) - _position));
+			intersection->setPosition(ray.pointAtParameter(t1));
+			intersection->setMaterial(_material);
+			intersection->t = t1;
+			return true;
+		}
+		if (t2 > D_HIT_MIN && intersection->t >= t2)
+		{
+			intersection->setNormal(normalize(ray.pointAtParameter(t2) - _position));
+			intersection->setPosition(ray.pointAtParameter(t2));
+			intersection->setMaterial(_material);
+			intersection->t = t2;
+			return true;
+		}
+	}
+	return false;
+}
+
+/*
+Triangle
+*/
+
+Triangle::Triangle(const Vec3& vertex_a,
+	const Vec3& vertex_b,
+	const Vec3& vertex_c,
+	Material* material) :
+	_vertex_a(vertex_a),
+	_vertex_b(vertex_b),
+	_vertex_c(vertex_c),
+	_material(material)
+{
+	_edge_ab = _vertex_b - _vertex_a;
+	_edge_ac = _vertex_c - _vertex_a;
+
+	_position = Vec3((_vertex_a.x() + _vertex_b.x() + _vertex_c.x()) / 3,
+		(_vertex_a.y() + _vertex_b.y() + _vertex_c.y()) / 3,
+		(_vertex_a.z() + _vertex_b.z() + _vertex_c.z()) / 3);
+
+	//三角形を構成する3頂点の各軸の最大最小からAABBを構築する
+	double max_x = std::fmax(_vertex_a.x(), std::fmax(_vertex_b.x(), _vertex_c.x()));
+	double max_y = std::fmax(_vertex_a.y(), std::fmax(_vertex_b.y(), _vertex_c.y()));
+	double max_z = std::fmax(_vertex_a.z(), std::fmax(_vertex_b.z(), _vertex_c.z()));
+	double min_x = std::fmin(_vertex_a.x(), std::fmin(_vertex_b.x(), _vertex_c.x()));
+	double min_y = std::fmin(_vertex_a.y(), std::fmin(_vertex_b.y(), _vertex_c.y()));
+	double min_z = std::fmin(_vertex_a.z(), std::fmin(_vertex_b.z(), _vertex_c.z()));
+	_bounding_box = new AABB(Vec3(max_x, max_y, max_z),
+		Vec3(min_x, min_y, min_z));
+
+	Vec3 cross_vec = cross(_edge_ab, _edge_ac);
+	_normal = normalize(cross_vec);
+	_surface_area = cross_vec.length() / 2.0;
+}
+
+Triangle::~Triangle()
+{
+	delete _bounding_box;
+	delete _material;
+}
+
+Material* Triangle::material() const
+{
+	return _material;
+}
+
+AABB* Triangle::boundingBox() const
+{
+	return _bounding_box;
+}
+
+Vec3 Triangle::position() const
+{
+	return _position;
+}
+
+double Triangle::surfaceArea() const
+{
+	return _surface_area;
+}
+
+Vec3 Triangle::randomPoint(Random* random) const
+{
+	const auto rnd = random->zeroToOneFloat();
+	return _edge_ab * rnd + _edge_ab * (1.0 - rnd);
+}
+
+//行列式算出関数
+//クラメルの公式で、三角形上の交差位置を求める際に使用する
+/*
+具体的な公式は
+x + y + z = 9
+2x + 3y -2z = 5
+3x -y + z = 7
+という連立方程式があった際に、各変数の係数をベクトルとしてまとめて、
+(1, 2, 3)*x + (1,3,-1)*y + (1,-2,1)*z = (9,5,7)とする。この時xの係数をaとする。b,cも同様である
+x = det(d, b, c)/det(a, b, c)で求まる。
+
+*/
+double Triangle::det(Vec3 a, Vec3 b, Vec3 c) const
+{
+	return (a.x()*b.y()*c.z()) + (a.y()*b.z()*c.x()) + (a.z()*b.x()*c.y()) - (a.x()*b.z()*c.y()) - (a.y()*b.x()*c.z()) - (a.z()*b.y()*c.x());
+}
+
+/*
+三角形の当たり判定は、vertex0 + edgeA * u + edgeB * v = ray.org + ray.dir * tで求められる
+ここでedgeA,Bは、それぞれ三角形の一頂点であるvertex0から残り2つの頂点へと伸びた線分
+上記の式を、係数の無いものとあるもので整理すると、
+edgeA * u + edgeB * v - ray.dir * t = ray.org - vertex0
+ここで、係数u,v,tをクラメルの公式を使用して求める
+*/
+inline bool Triangle::intersect(const Ray& ray, Intersection *intersection) const
+{
+	//公式の関係で、rayは逆向き
+	Vec3 inv_raydir = normalize(Vec3(-ray.direction()));
+	//(a,b,c)この後の計算の共通分母になる部分.
+	//a,b,cで作られる体積で、この後出て来る体積を割って、uvtを求める
+	double denominator = det(_edge_ab, _edge_ac, inv_raydir);
+	//レイが平面と並行でない場合
+	if (denominator > 0)
+	{
+		Vec3 vertex_a_pos = ray.origin() - _vertex_a;
+
+		//(d,b,c)
+		double u = det(vertex_a_pos, _edge_ac, inv_raydir) / denominator;
+		//uが範囲外だったら何もしない
+		if (0.0 <= u && u <= 1.0)
+		{
+			//(a,d,c)
+			double v = det(_edge_ab, vertex_a_pos, inv_raydir) / denominator;
+			if (0.0 <= v && u + v <= 1.0)
+			{
+				//(a,b,d)
+				double t = det(_edge_ab, _edge_ac, vertex_a_pos) / denominator;
+				if (t < 0 || t > intersection->t)
+				{
+					return false;
+				}
+				intersection->setNormal(_normal);
+				intersection->setPosition(ray.pointAtParameter(t));
+				intersection->setMaterial(material());
+				intersection->t = t;
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+}//namespace LtRenderer
